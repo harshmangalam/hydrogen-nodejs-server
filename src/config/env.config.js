9fe6ac6c030ff7fd31d126fcfe1d4b8ec2950920
harshmangalam <harshdev8218@gmail.com>
2022-09-30 20:40:23 +0530

    added env variables

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -1,5 +1,5 @@
 const PORT = process.env.PORT || "5000";
-const ORIGIN = process.env.ORIGIN || `["http://localhost:3000"]`;
+const ORIGIN = process.env.ORIGIN || "http://localhost:3000";
 const JWT_SECRET = process.env.JWT_SECRET || "itssecret";
 const NODE_ENV = process.env.NODE_ENV || "dev";
 module.exports = {
