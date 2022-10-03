2faaea4e895a47f972aa75336a2f939844df47de
harshmangalam <harshdev8218@gmail.com>
2022-10-03 07:27:58 +0530

    added changes in delete comment

--- src/controllers/comments/deleteComment.js
+++ src/controllers/comments/deleteComment.js
@@ -1,23 +1,23 @@
 const { db } = require("../../utils/db");
 exports.deleteComment = async (req, res, next) => {
-	try {
-		const commentId = req.params.commentId;
+  try {
+    const commentId = req.params.commentId;
 
-		const comment = await db.comment.delete({
-			where: {
-				id: commentId,
+    const comment = await db.comment.delete({
+      where: {
+        id: commentId,
+      },
+      select: {
+        id: true,
+      },
+    });
 
-			}
-		})
-
-		return res.status(201).json({
-			type: "success",
-			message: "Comment deleted",
-			data: {
-				comment,
-			},
-		});
-	} catch (error) {
-		next(error);
-	}
+    return res.status(201).json({
+      type: "success",
+      message: "Comment deleted",
+      data: null,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
